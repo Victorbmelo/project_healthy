e32eaf17f38a24a0315b2d99b4c02828b68574b3
Victor cajubá de Britto Bacelar Caldas <victorcajuba@gmail.com>
2024-09-13 20:09:13 +0200

    Uploading Lamp Controle Service
    Responsible to manage database of lamp settings

---
+++ lampControl.py
@@ -0,0 +1,64 @@
+import cherrypy
+import json
+import sqlite3
+
+class LampAction:
+    exposed = True
+
+    def PUT(self, *uri, **params):
+        # Database connection
+        conn = sqlite3.connect('D:/Documents/node/sqlite-tools-win-x64-3460100/lamp_schedule.db')  # Replace with your actual database path
+        reqString = cherrypy.request.body.read().decode()  # Read request body and decode from binary to string
+        reqDict = json.loads(reqString)  # Convert JSON string to dictionary
+        print(reqDict)  # Print the received data for debugging
+
+        cursor = conn.cursor()
+        
+        # Extract values from request
+        time = reqDict['time']
+        weekdays = ','.join(reqDict.get('weekdays', []))  # Default to empty list if 'weekdays' is not provided
+        state = reqDict['state']
+
+        # Check if record already exists
+        print("weekdays")
+        print(weekdays)
+        check_query = """SELECT COUNT(*) FROM schedules WHERE time = ? AND weekday = ?"""
+        cursor.execute(check_query, (time,weekdays))
+        record_exists = cursor.fetchone()[0] > 0
+        
+        if record_exists:
+            # Record exists, return a message
+            cherrypy.response.status = 409  # Conflict status code
+            #return json.dumps({"status": "exists", "message": "Record already exists"})
+            return "Record already exists"
+        else:
+            # Insert the record
+            print("There is not this record ")
+            insert_query = """INSERT INTO schedules(time, weekday, state) VALUES (?, ?, ?)"""
+            cursor.execute(insert_query, (reqDict['time'], ','.join(reqDict['weekdays']), reqDict['state']))
+
+            conn.commit()
+            
+            # Return a success response
+            cherrypy.response.status = 200
+            #return json.dumps({"status": "success", "message": "Data inserted successfully"})
+            return  "Data inserted successfully"
+        conn.close()
+
+if __name__ == "__main__":
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+        }
+    }
+    # Mounting the application
+    cherrypy.tree.mount(LampAction(), '/test', conf)
+
+    # Server configuration
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'})  # Listening on all available network interfaces
+    cherrypy.config.update({'server.socket_port': 8080})  # Set the port number
+
+    # Start CherryPy engine
+    cherrypy.engine.start()
+    cherrypy.engine.block()
