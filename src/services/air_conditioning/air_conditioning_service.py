891623f3fbc486e1b668fd1d77eee0a23183ccab
victormel0 <victor.bezerramelo@deepware.it>
2025-02-16 22:20:17 +0100

    Implement update last readings in services

--- src/services/air_conditioning/air_conditioning_service.py
+++ src/services/air_conditioning/air_conditioning_service.py
@@ -1,42 +1,66 @@
+import datetime
 import json
+import requests
 from src.mqtt.mqtt_handler import MqttHandler
 
+API_BASE_URL = 'http://localhost:8080'
+
+
 class AirConditioningService:
-    MQTT_TOPIC = '/+/airconditioning/+/+/+'
+    MQTT_TOPIC = '+/air_conditioning/+/+'
 
     def __init__(self):
         pass
 
     def on_message(self, client, userdata, msg):
         rcvd_topic = msg.topic
-        send_topic = f"{rcvd_topic}/alert"
+        patient_id, service_name, device_id, entity_id = rcvd_topic.lstrip('/').split('/')
+        send_topic = f"{patient_id}/alert"
         payload = json.loads(msg.payload.decode())
-        print(f"Received: {payload} on {rcvd_topic}")
-        data = {'data': payload, 'alert_message': ""}
+        print(f"[AirConditioningService] Received: {payload} on {rcvd_topic}")
 
-        if 'temperature' in payload:
-            temperature = payload['temperature']
-            if temperature > 25:
-                alert_str = "Temperature too high, adjusting air conditioning..."
-                data["alert_message"] = alert_str
-                print(alert_str)
-                client.publish(send_topic, json.dumps(data))
-            elif temperature < 18:
-                alert_str = "Temperature too low, adjusting air conditioning..."
-                data["alert_message"] = alert_str
-                print(alert_str)
-                client.publish(send_topic, json.dumps(data))
-            else:
-                print("Temperature is optimal, keeping current settings.")
+        if isinstance(payload, int):
+            humidity = payload
+        else:
+            try:
+                humidity = int(payload)
+            except:
+                return
+        timestamp_now = datetime.datetime.now(datetime.timezone.utc).isoformat(timespec='seconds')
+        request_payload = {
+            'last_reading': str(humidity),
+            'last_reading_timestamp': timestamp_now
+        }
+        r = requests.put(f"{API_BASE_URL}/entity?entity_id={entity_id}", json=request_payload)
+        print(f"[AirConditioningService] PUT Request entity_id: {entity_id}, response: {r}")
+
+        data = {
+            "patient_id": int(patient_id),
+            "entity_id": int(entity_id),
+            "value": humidity,
+            "timestamp": timestamp_now,
+            "message": None
+        }
+
+        if humidity > 65:
+            alert_str = f"Humidity {humidity}% too high..."
+            data["message"] = alert_str
+            print("[AirConditioningService] " + alert_str)
+            client.publish(send_topic, json.dumps(data))
+        elif humidity < 15:
+            alert_str = f"Humidity {humidity}% too low..."
+            data["message"] = alert_str
+            print("[AirConditioningService] " + alert_str)
+            client.publish(send_topic, json.dumps(data))
 
     def start(self):
         mqtt = MqttHandler(client_id='air-conditioning-service')
         mqtt._client.on_message = self.on_message
         mqtt.connect()
         mqtt.subscribe(self.MQTT_TOPIC)
-        print(f"Air Conditioning Service started and subscribed to {self.MQTT_TOPIC}")
+        print(f"[AirConditioningService] Air Conditioning Service started and subscribed to {self.MQTT_TOPIC}")
         try:
             pass
         except KeyboardInterrupt:
             mqtt.close()
-            print("Air Conditioning Service stopped.")
+            print("[AirConditioningService] Air Conditioning Service stopped.")
--- src/services/blood_pressure/blood_pressure_service.py
+++ src/services/blood_pressure/blood_pressure_service.py
@@ -1,35 +1,60 @@
+import datetime
 import json
+import requests
 from src.mqtt.mqtt_handler import MqttHandler
 
+API_BASE_URL = 'http://localhost:8080'
+
+
 class BloodPressureService:
-    MQTT_TOPIC = '/+/bloodpressure/+/+/+'
+    MQTT_TOPIC = '+/blood_pressure/+/+'
 
     def __init__(self):
         pass
 
     def on_message(self, client, userdata, msg):
         rcvd_topic = msg.topic
-        send_topic = f"{rcvd_topic}/alert"
+        patient_id, service_name, device_id, entity_id = rcvd_topic.lstrip('/').split('/')
+        send_topic = f"{patient_id}/alert"
         payload = json.loads(msg.payload.decode())
-        print(f"Received: {payload} on {rcvd_topic}")
-        data = {'data': payload, 'alert_message': ""}
+        print(f"[BloodPressureService] Received: {payload} on {rcvd_topic}")
 
-        if 'bloodPressure' in payload:
-            blood_pressure = payload['bloodPressure']
-            if blood_pressure > 140 or blood_pressure < 60:
-                alert_str = "Abnormal blood pressure detected! Sending alert..."
-                data["alert_message"] = alert_str
-                print(alert_str)
-                client.publish(send_topic, json.dumps(data))
+        if isinstance(payload, int):
+            blood_pressure = payload
+        else:
+            try:
+                blood_pressure = int(payload)
+            except:
+                return
+        timestamp_now = datetime.datetime.now(datetime.timezone.utc).isoformat(timespec='seconds')
+        request_payload = {
+            'last_reading': str(blood_pressure),
+            'last_reading_timestamp': timestamp_now
+        }
+        r = requests.put(f"{API_BASE_URL}/entity?entity_id={entity_id}", json=request_payload)
+        print(f"[BloodPressureService] PUT Request entity_id: {entity_id}, response: {r}")
+
+        data = {
+            "patient_id": int(patient_id),
+            "entity_id": int(entity_id),
+            "value": blood_pressure,
+            "timestamp": timestamp_now,
+            "message": None
+        }
+        if blood_pressure > 140 or blood_pressure < 60:
+            alert_str = "Abnormal blood pressure detected! Sending alert..."
+            data["message"] = alert_str
+            print("[BloodPressureService] " + alert_str)
+            client.publish(send_topic, json.dumps(data))
 
     def start(self):
         mqtt = MqttHandler(client_id='blood-pressure-service')
         mqtt._client.on_message = self.on_message
         mqtt.connect()
         mqtt.subscribe(self.MQTT_TOPIC)
-        print(f"Blood Pressure Service started and subscribed to {self.MQTT_TOPIC}")
+        print(f"[BloodPressureService] Blood Pressure Service started and subscribed to {self.MQTT_TOPIC}")
         try:
             pass
         except KeyboardInterrupt:
             mqtt.close()
-            print("Blood Pressure Service stopped.")
+            print("[BloodPressureService] Blood Pressure Service stopped.")
